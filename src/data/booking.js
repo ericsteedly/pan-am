8c14cc260d0ca312eb69e64247b766d5134b6539
Eric Steedly <ericsteedly@gmail.com>
2026-04-19 13:37:37 -0500

    Bugfix on delete_booking api call method. Move Id from body to URL. Bug revieled from change in api code

--- src/data/booking.js
+++ src/data/booking.js
@@ -1,92 +1,88 @@
-import { fetchWithResponse, fetchWithoutResponse } from "./fetcher"; 
+import { fetchWithResponse, fetchWithoutResponse } from "./fetcher";
 
 export const createBooking = (flights) => {
-    return fetchWithResponse("bookings", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Token ${localStorage.getItem("token")}`
-        },
-        body: JSON.stringify(flights)
-    })
-}
+  return fetchWithResponse("bookings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(flights),
+  });
+};
 
 export const createRoundTrip = (roundTripObj) => {
-    return fetchWithResponse("bookings/roundtrip", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Token ${localStorage.getItem("token")}`
-        },
-        body: JSON.stringify(roundTripObj)
-    })
-}
+  return fetchWithResponse("bookings/roundtrip", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(roundTripObj),
+  });
+};
 
 export const deleteBooking = (bookingId) => {
-    return fetchWithoutResponse("bookings", {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Token ${localStorage.getItem("token")}`
-        },
-        body: JSON.stringify (bookingId)
-    })
-}
+  return fetchWithoutResponse(`bookings/${bookingId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+  });
+};
 
 export const editBooking = (paymentObj, bookingId) => {
-    return fetchWithoutResponse(`bookings/${bookingId}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Token ${localStorage.getItem("token")}`
-        },
-        body: JSON.stringify (paymentObj)
-    })
-}
+  return fetchWithoutResponse(`bookings/${bookingId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(paymentObj),
+  });
+};
 
 export const listBookings = () => {
-    return fetchWithResponse("bookings", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Token ${localStorage.getItem("token")}`
-        }
-    })
-}
-
+  return fetchWithResponse("bookings", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+  });
+};
 
 export const retrieveBooking = async (bookingId) => {
-    return await fetch(`https://panamapi.dev/bookings/${bookingId}`,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Token ${localStorage.getItem("token")}`
-            }
-        }
-    ).then((res)=>res.json())
-}
+  return await fetch(`https://panamapi.dev/bookings/${bookingId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+  }).then((res) => res.json());
+};
 
 export const retrieveRoundTrip = async (roundTripId) => {
-    return await fetch(`https://panamapi.dev/bookings/${roundTripId}/retrieveRoundtrip`,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Token ${localStorage.getItem("token")}`
-            }
-        }
-    ).then((res)=>res.json())
-}
+  return await fetch(
+    `https://panamapi.dev/bookings/${roundTripId}/retrieveRoundtrip`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.getItem("token")}`,
+      },
+    },
+  ).then((res) => res.json());
+};
 
 export const deleteRoundtrip = (roundtripObj) => {
-    return fetchWithoutResponse("bookings/roundtrip", {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Token ${localStorage.getItem("token")}`
-        },
-        body: JSON.stringify (roundtripObj)
-    })
-}
-
+  return fetchWithoutResponse("bookings/roundtrip", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(roundtripObj),
+  });
+};
